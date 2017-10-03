b5d2022fb3de2cd51bac26452ec148b17e54ebbc
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-03 12:23:54 -0600

    I forgot to save. :-1:

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -2,5 +2,8 @@ package list.controller;
 
 public class ListController
 {
-
+	public void start()
+	{
+		
+	}
 }
--- src/list/controller/ListRunner.java
+++ src/list/controller/ListRunner.java
@@ -2,5 +2,9 @@ package list.controller;
 
 public class ListRunner
 {
-
+	public static void main (String [] args)
+	{
+		ListController app = new ListController();
+		app.start();
+	}
 }
